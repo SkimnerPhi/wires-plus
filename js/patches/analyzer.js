e7a87d1583df47fc4cb568e4b9d8005a25885dac
Lepeltier kévin <kevin lepeltier [aka lipki]>
2022-03-24 18:55:19 +0100

    Repair of the shape analyzer

--- js/patches/analyzer.js
+++ js/patches/analyzer.js
@@ -5,12 +5,35 @@ import { enumPinSlotType } from "shapez/game/components/wired_pins";
 import { generateMatrixRotations } from "shapez/core/utils";
 import { unCombineDefinitions } from "../compat/shapez_industries";
 import { LogicGateSystem } from "shapez/game/systems/logic_gate";
+import { enumLogicGateType } from "shapez/game/components/logic_gate";
+import { defaultBuildingVariant } from "shapez/game/meta_building";
 
 export function patchAnalyzer() {
     if (isModLoaded("shapez-industries")) {
-        this.modInterface.addVariantToExistingBuilding(MetaAnalyzerBuilding, "uncombiner", {
+
+        const enumAnalyzerVariants = {
+            uncombiner: "uncombiner",
+        };
+
+        this.modInterface.extendClass(MetaAnalyzerBuilding, ({ $old }) => ({
+            enumAnalyzerVariants: {
+                [defaultBuildingVariant]: enumLogicGateType.analyzer,
+                analyzer: enumLogicGateType.analyzer,
+                uncombiner: enumAnalyzerVariants.uncombiner,
+            },
+        }));
+
+        Object.assign(enumLogicGateType, enumAnalyzerVariants);
+
+        const enumVariantToGate = {
+            [defaultBuildingVariant]: enumLogicGateType.analyzer,
+            [enumAnalyzerVariants.uncombiner]: enumLogicGateType.uncombiner,
+        };
+
+        this.modInterface.addVariantToExistingBuilding(MetaAnalyzerBuilding, enumAnalyzerVariants.uncombiner, {
             name: "Virtual UnCombiner",
-            description: "UnCombine the top right quadrant of the lowest layer of the shape and returns two uncolor shapes.",
+            description:
+                "UnCombine the top right quadrant of the lowest layer of the shape and returns two uncolor shapes.",
             isUnlocked(root) {
                 return root.hubGoals.isRewardUnlocked("reward_shape_combiner");
             },
@@ -18,8 +41,10 @@ export function patchAnalyzer() {
 
         this.modInterface.extendClass(MetaAnalyzerBuilding, ({ $old }) => ({
             updateVariants(entity, rotationVariant, variant) {
-                entity.components.LogicGate.type = "uncombiner";
-                entity.components.WiredPins.setSlots([
+                const gateType = enumVariantToGate[variant];
+                entity.components.LogicGate.type = gateType;
+                const pinComp = entity.components.WiredPins;
+                pinComp.setSlots([
                     {
                         pos: new Vector(0, 0),
                         direction: enumDirection.bottom,
@@ -54,16 +79,20 @@ export function patchAnalyzer() {
 
                 const firstDefinition = first.definition;
                 const unCombinedDefinition = unCombineDefinitions(firstDefinition);
-                return [this.root.shapeDefinitionMgr.getShapeItemFromDefinition(unCombinedDefinition[0]),
-                    this.root.shapeDefinitionMgr.getShapeItemFromDefinition(unCombinedDefinition[1])];
+                return [
+                    this.root.shapeDefinitionMgr.getShapeItemFromDefinition(unCombinedDefinition[0]),
+                    this.root.shapeDefinitionMgr.getShapeItemFromDefinition(unCombinedDefinition[1]),
+                ];
             },
         };
 
         this.modInterface.extendClass(LogicGateSystem, ({ $old }) => toExtend);
-    
+
         this.signals.gameInitialized.add(root => {
-                const siUnCombiner = root.systemMgr.systems.logicGate.compute_UNCOMBINE.bind(root.systemMgr.systems.logicGate);
-                root.systemMgr.systems.logicGate.boundOperations["uncombiner"] = siUnCombiner;
+            const siUnCombiner = root.systemMgr.systems.logicGate.compute_UNCOMBINE.bind(
+                root.systemMgr.systems.logicGate
+            );
+            root.systemMgr.systems.logicGate.boundOperations[enumLogicGateType.uncombiner] = siUnCombiner;
         });
     }
 }
